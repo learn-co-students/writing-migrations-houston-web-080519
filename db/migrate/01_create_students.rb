4ae4810806aa33044c151b8b08706a38e7d7925e
Richard Pitts <Richardepitts@gmail.com>
2019-08-15 17:06:58 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table :students2 do |t| 
+            t.string :name
+            t.integer :age
+            t.datatype :birthdate
+        end 
+    end
 end
